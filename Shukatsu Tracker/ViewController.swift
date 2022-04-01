c0cef8538ec2117d3f9308ab5e42348d1dcac254
moet-stein <0101.mste@gmail.com>
2022-04-01 14:56:35 +0200

    Initial Commit

---
+++ Shukatsu Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shukatsu Tracker
+//
+//  Created by Moe Steinmueller on 01.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
