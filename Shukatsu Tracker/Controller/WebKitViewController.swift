fad203bc474909bbd591cd7ed94a131fb1db3ec6
moet-stein <0101.mste@gmail.com>
2022-04-18 11:58:19 +0200

    create WebKitVC

---
+++ Shukatsu Tracker/Controller/WebKitViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WebKitViewController.swift
+//  Shukatsu Tracker
+//
+//  Created by Moe Steinmueller on 18.04.22.
+//
+
+import UIKit
+import WebKit
+
+class WebKitViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    var link: String
+    
+    init(link: String) {
+        self.link = link
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    
+}
