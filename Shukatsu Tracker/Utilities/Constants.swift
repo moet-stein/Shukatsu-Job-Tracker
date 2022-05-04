e8e0a95908f43daba6015ddfe92a102d2631b7f2
moet-stein <0101.mste@gmail.com>
2022-05-04 12:23:16 +0200

    create Fonts and Colors static constant in enum

--- Shukatsu Tracker/Utilities/Constants.swift
+++ Shukatsu Tracker/Utilities/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum JobStatus: String {
     case open = "open"
@@ -14,3 +15,19 @@ enum JobStatus: String {
     case closed = "closed"
 }
 
+enum Fonts {
+    static let latoLight = "Lato-Light"
+    static let latoRegular = "Lato-Regular"
+    static let latoBold = "Lato-Bold"
+    static let latoBlack = "Lato-Black"
+}
+
+enum Colors {
+    static let skyBlue = UIColor(named: "skyBlue")
+    static let bgOffWhite = UIColor(named: "bfOffWhite")
+    static let blueGrey = UIColor(named: "blueGrey")
+    static let lightGreen = UIColor(named: "lightGreen")
+    static let lightOrange = UIColor(named: "lightOrange")
+    static let softBrown = UIColor(named: "softBrown")
+    static let viewOrange = UIColor(named: "viewOrange")
+}
