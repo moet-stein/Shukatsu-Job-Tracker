148c501bfb24ce00f153817dd6525a94276a7bd8
moet-stein <0101.mste@gmail.com>
2022-06-12 15:49:21 +0200

    create viewModel unitTest file

--- ShukatsuTrackerTests/JobInfoViewModelTests.swift
+++ ShukatsuTrackerTests/JobInfoViewModelTests.swift
@@ -6,30 +6,25 @@
 //
 
 import XCTest
+@testable import Shukatsu_Tracker
 
 class JobInfoViewModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var jobInfoDataManager: JobInfoDataManager!
+    var coreDataStack: CoreDataStack!
+
+    override func setUp() {
+      super.setUp()
+      coreDataStack = TestCoreDataStack()
+        jobInfoDataManager = JobInfoDataManager(
+        managedObjectContext: coreDataStack.mainContext,
+        coreDataStack: coreDataStack)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    override func tearDown() {
+      super.tearDown()
+      jobInfoDataManager = nil
+      coreDataStack = nil
     }
 
 }
