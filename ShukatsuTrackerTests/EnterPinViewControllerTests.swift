f260222433f3ab8f3f44ffb2a97b258df3391375
moet-stein <0101.mste@gmail.com>
2022-07-05 17:10:04 +0200

    implement test method testIsPinMatched_whenIlligalCharacterUsedInPassword_pinShouldNotMatch

--- ShukatsuTrackerTests/EnterPinViewControllerTests.swift
+++ ShukatsuTrackerTests/EnterPinViewControllerTests.swift
@@ -9,22 +9,32 @@ import XCTest
 @testable import Shukatsu_Tracker
 
 class EnterPinViewControllerTests: XCTestCase {
+    
+    var sut: EnterPinViewController!
+    
     override func setUp() {
-        
+        sut = EnterPinViewController()
     }
     
     override func tearDown() {
-        
+        sut = nil
     }
     
     func testIsPinMatched_whenIlligalCharacterUsedInPassword_pinShouldMatch() {
         let enteredPin = "1234"
         let storedPin = "1234"
         
-        let sut = EnterPinViewController()
-        
         let isPinMatched = sut.isPinMatched(enteredPin: enteredPin, storedPin: storedPin)
         
         XCTAssertTrue(isPinMatched)
+    }
+    
+    func testIsPinMatched_whenIlligalCharacterUsedInPassword_pinShouldNotMatch() {
+        let enteredPin = "1234"
+        let storedPin = "1233"
+        
+        let isPinMatched = sut.isPinMatched(enteredPin: enteredPin, storedPin: storedPin)
+        
+        XCTAssertFalse(isPinMatched)
     }
 }
